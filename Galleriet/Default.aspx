72bc8533b35d2a904b7b29c28a889a3813aa79b3
mf222nb <mf222nb@student.lnu.se>
2014-02-11 17:01:39 +0100

    börjat med laborationen

---
+++ Galleriet/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Galleriet.Default" ViewStateMode="Disabled"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <header>
+        <h1>Galleriet</h1>
+    </header>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Repeater ID="Repeater1" runat="server">
+            <ItemTemplate>
+                <asp:HyperLink ID="HyperLink1" runat="server">HyperLink</asp:HyperLink>
+            </ItemTemplate>
+        </asp:Repeater>
+        
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="RequiredFieldValidator"></asp:RequiredFieldValidator>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ErrorMessage="RegularExpressionValidator"></asp:RegularExpressionValidator>  
+        <asp:Button ID="ChoseButton" runat="server" Text="Välj fil" Onclick="ChoseButton_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Galleriet/Model/Gallery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Galleriet.Model
+{
+    public class Gallery
+    {
+        private static Regex ApprovedExenstions;
+        private static Regex SantizePath;
+        private static string PhysicalUploadedImagePath;
+
+        static Gallery()
+        {
+            ApprovedExenstions = new Regex("^.*/.(gif|jpg|png)$", RegexOptions.IgnoreCase);
+            
+            var invalidChars = new string(Path.GetInvalidFileNameChars());
+            SantizePath = new Regex(string.Format("[0]", Regex.Escape(invalidChars)));
+
+            PhysicalUploadedImagePath = Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), "Content/Images");
+        }
+
+        public IEnumerable<string> GetImageNames()
+        {
+            var di = new DirectoryInfo(PhysicalUploadedImagePath);
+            throw new NotImplementedException();
+        }
+
+        public bool ImageExists(string name)
+        {
+            if (!File.Exists(name))
+            {
+                return false;
+            }
+            else
+	        {
+                return true;
+	        }
+        }
+
+        private bool IsValidImage(Image image)
+        {
+            return image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Gif.Guid ||
+                   image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Jpeg.Guid ||
+                   image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Png.Guid;
+        }
+
+        public string SaveImage(Stream stream, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
